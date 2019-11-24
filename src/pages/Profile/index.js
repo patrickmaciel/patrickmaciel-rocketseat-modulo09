e2ea3b0fc4d4c30e1cdbd80f9653c1089c758d07
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-11-23 22:12:14 -0300

    atualizando perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
+
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import { Container } from './styles';
 
 export default function Profile() {
   const profile = useSelector(state => state.user.profile);
 
-  function handleSubmit(data) {}
+  const dispatch = useDispatch();
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
 
   return (
     <Container>
